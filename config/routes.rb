5db1eb6f253a143afb5f4bc20d767b221b391d2b
Thibault Mercier <thib.mercier@ymail.com>
2023-02-22 16:43:23 +0100

    Yelp MVP done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
+category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+10.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category.sample)
+end
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
